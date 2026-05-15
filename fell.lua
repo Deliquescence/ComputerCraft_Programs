ec1574ba761f079b806f33510749e9c92b9346c5
Josh Baird <jjb.pub01@protonmail.com>
2026-05-14 21:53:56 -0400

    Combine fell programs

--- fell.lua
+++ fell.lua
@@ -1,50 +1,69 @@
 local tArgs = { ... }
+---@diagnostic disable-next-line: undefined-field
+os.loadAPI("JoshAPI.lua")
 
-local height = 1
+local function fell2()
+  local height = 1
 
-function fuel()
-  if turtle.getFuelLevel() == 0 then
-    turtle.refuel(1)
-  end
-end
-
-turtle.dig()
-fuel()
-turtle.forward()
-while turtle.detectUp() do
   turtle.dig()
-  turtle.digUp()
-  fuel()
-  turtle.up()
-  height = height+1
-end
-turtle.turnRight()
-turtle.dig()
-fuel()
-turtle.forward()
-turtle.turnLeft()
-while height > 1 do
-  turtle.dig()
-  turtle.digDown()
-  fuel()
-  turtle.down()
-  height = height - 1
-end
-turtle.dig()  
-fuel()
-turtle.back()
-turtle.turnLeft()
-fuel()
-turtle.forward()
-turtle.turnRight()
-
-if tostring(tArgs[1])=="chest" then
-  turtle.turnLeft()
-  for i=1,16 do
-    turtle.select(i)
-    if turtle.getItemCount(i) > 0 then
-      turtle.drop()
-    end
+  JoshAPI.forward()
+  while turtle.detectUp() or turtle.detect() do
+    turtle.dig()
+    turtle.digUp()
+    JoshAPI.up()
+    height = height + 1
   end
   turtle.turnRight()
+  turtle.dig()
+  JoshAPI.forward()
+  turtle.turnLeft()
+  while height > 1 do
+    turtle.dig()
+    turtle.digDown()
+    JoshAPI.down()
+    height = height - 1
+  end
+  turtle.dig()
+  JoshAPI.back()
+  turtle.turnLeft()
+  JoshAPI.forward()
+  turtle.turnRight()
+
+  if tostring(tArgs[1]) == "chest" or tostring(tArgs[2]) == "chest" then
+    turtle.turnLeft()
+    for i = 1, 16 do
+      turtle.select(i)
+      if turtle.getItemCount(i) > 0 then
+        turtle.drop()
+      end
+    end
+    turtle.turnRight()
+  end
+end
+
+local function fell1()
+  local height = 1
+
+  turtle.dig()
+  JoshAPI.forward()
+  while turtle.detectUp() do
+    turtle.digUp()
+    JoshAPI.up()
+    height = height + 1
+  end
+
+  while height > 1 do
+    turtle.digDown()
+    JoshAPI.down()
+    height = height - 1
+  end
+end
+
+local which = tostring(tArgs[1])
+if which == "1" or which == "small" then
+  fell1()
+elseif which == "2" or which == "chest" then
+  fell2()
+else
+  fell2()
 end
